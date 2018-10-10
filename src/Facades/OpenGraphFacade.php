7b73c754c4643239a4fcade1b9b2a09a0f013401
Shashi Prakash Gautam <contactmespg@gmail.com>
2018-10-10 21:33:33 +0000

    Apply fixes from StyleCI

--- src/Facades/OpenGraphFacade.php
+++ src/Facades/OpenGraphFacade.php
@@ -4,10 +4,10 @@ namespace shweshi\OpenGraph\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class OpenGraphFacade extends Facade {
-
-    protected static function getFacadeAccessor() {
+class OpenGraphFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
         return 'OpenGraph';
     }
-
 }
--- src/Providers/OpenGraphProvider.php
+++ src/Providers/OpenGraphProvider.php
@@ -4,14 +4,15 @@ namespace shweshi\OpenGraph\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class OpenGraphProvider extends ServiceProvider {
-
+class OpenGraphProvider extends ServiceProvider
+{
     /**
      * Bootstrap the application services.
      *
      * @return void
      */
-    public function boot() {
+    public function boot()
+    {
         //
     }
 
@@ -20,10 +21,10 @@ class OpenGraphProvider extends ServiceProvider {
      *
      * @return void
      */
-    public function register() {
-        $this->app->bind('OpenGraph', function() {
-            return new \shweshi\OpenGraph\OpenGraph;
+    public function register()
+    {
+        $this->app->bind('OpenGraph', function () {
+            return new \shweshi\OpenGraph\OpenGraph();
         });
     }
-
 }
