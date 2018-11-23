02e5f9ba67c4e905534fc767a58f9bed22631c79
Shashi Prakash Gautam <contactmespg@gmail.com>
2018-11-23 18:18:17 +0000

    Apply fixes from StyleCI

--- tests/OpenGraphTest.php
+++ tests/OpenGraphTest.php
@@ -9,26 +9,26 @@ class OpenGraphTest extends TestCase
 {
     /** @test */
     public function testFetch()
-   {
+    {
         $o = new OpenGraph();
-       $data = $o->fetch(
+        $data = $o->fetch(
          'https://www.unsplash.com/'
        );
-       $this->assertArrayHasKey('title', $data);
-       $this->assertArrayHasKey('description', $data);
-       $this->assertArrayHasKey('type', $data);
-       $this->assertArrayHasKey('url', $data);
-       $this->assertArrayHasKey('image', $data);
-   }
+        $this->assertArrayHasKey('title', $data);
+        $this->assertArrayHasKey('description', $data);
+        $this->assertArrayHasKey('type', $data);
+        $this->assertArrayHasKey('url', $data);
+        $this->assertArrayHasKey('image', $data);
+    }
 
-   /** @test */
-   public function testFetchReturnsEmptyArrayForWebsiteWithNoMetadata()
-  {
-       $o = new OpenGraph();
-      $data = $o->fetch(
+    /** @test */
+    public function testFetchReturnsEmptyArrayForWebsiteWithNoMetadata()
+    {
+        $o = new OpenGraph();
+        $data = $o->fetch(
         'https://www.example.com/'
       );
 
-      $this->assertEmpty($data);
-  }
+        $this->assertEmpty($data);
+    }
 }
